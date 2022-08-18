8757d10cb2671bcc2db14bf9bdc4ddaaac60dfcf
Jobsidney <jobsidney67@gmail.com>
2022-08-18 19:30:12 +0300

    commit tree 883a0d5ea3a640e969b4470b5c79cbd499bc0657

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,26 @@
 require 'pry'
 
 class String
+  attr_accessor :str
+  def initialize(str)
+    @str=str
+  end
 
   def sentence?
-    
+    str.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self.scan(/(\w)./)
   end
 end
+neww=String.new("job?")
+p neww.sentence?
